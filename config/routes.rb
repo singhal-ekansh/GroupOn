393a5b0f49def57e298c92280e27ca5efd2a3316
Ekansh Singhal <ekansin05@gmail.com>
2023-05-11 20:06:14 +0530

    user register form and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, path: "signup", only: [:create, :new, :show]
+  root "users#new"
 end
