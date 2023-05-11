054049181262412c71852d4c69a18b434c136366
Ekansh Singhal <ekansin05@gmail.com>
2023-05-11 15:28:37 +0530

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, format: {
+    with: URI::MailTo::EMAIL_REGEXP,
+    message: "syntax is not valid"
+  }
 end
