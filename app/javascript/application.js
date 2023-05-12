a0f7e14029afccfb2944687b0aa2713447a07f78
Ekansh Singhal <ekansin05@gmail.com>
2023-05-12 19:49:18 +0530

    webpacker installed and bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+<<<<<<< HEAD
 import "./controllers"
+=======
+import "controllers"
+>>>>>>> ee6adfc (webpacker installed and bootstrap)
 import 'bootstrap';
