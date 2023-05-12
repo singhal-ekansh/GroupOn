aac76129e84ddd67ad98b26cd37b122329a1b85a
Ekansh Singhal <ekansin05@gmail.com>
2023-05-13 01:31:03 +0530

    login funtionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    
+    if user&.authenticate(params[:password])
+      unless user.verified_at
+        redirect_to new_session_url, alert: "email not verified, click on the link sent to your email"
+      else
+        session[:user_id] = user.id
+        debugger
+      # redirect_to 
+      end
+    else
+      redirect_to new_session_url, alert: "Invalid user/password combination"
+    end
   end
 
 end
