ee6adfc9db939b765d8b937c0a5f364d323351ce
Ekansh Singhal <ekansin05@gmail.com>
2023-05-12 19:49:18 +0530

    webpacker installed and bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-  validates :first_name, :lastname, presence: true
+  validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :email, format: {
     with: URI::MailTo::EMAIL_REGEXP,
     message: "syntax is not valid"
   }, allow_blank: true
 
-=======
->>>>>>> b91a592 (users model schema)
   has_secure_password
 end
