6e81cb48a1856bd534dad6d7a14959496947d588
Ekansh Singhal <ekansin05@gmail.com>
2023-05-13 15:58:50 +0530

    login and forget passworf

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :users, only: [:create, :new, :show]
   get "/user-verify", to: "users#verify_user"
+  get "/reset-password-email", to: "users#forget_password_get"
+  post "/reset-password-email", to: "users#forget_password_post"
+
+  get "/reset-password", to: "users#reset_password_get"
+  post "/reset-password", to: "users#reset_password_post"
 
   resources :sessions, only: [:create, :new]
   root "users#new"
