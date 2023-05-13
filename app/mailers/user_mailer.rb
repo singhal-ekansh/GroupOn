413409a0792a373d696bd22e4b56587e2102355b
Ekansh Singhal <ekansin05@gmail.com>
2023-05-13 15:58:50 +0530

    login and forget passworf

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,12 @@
 class UserMailer < ApplicationMailer
   default from: "from@example.com"
   def email_verification(user)
-    @token = user.signed_id(purpose: 'email_verification', expires_in: 2.minutes)
+    @token = user.signed_id(purpose: 'email_verification', expires_in: 5.minutes)
     mail to: user.email, subject: 'GroupOn register verification'
+  end
+
+  def reset_password(user)
+    @token = user.signed_id(purpose: 'reset_password', expires_in: 5.minutes)
+    mail to: user.email, subject: 'GroupOn account recovery'
   end
 end
