50571dd75b8f9299485d8ffbafe77b2e6fb8a4ed
Ekansh Singhal <ekansin05@gmail.com>
2023-05-15 14:53:57 +0530

    extracted function in user model

--- app/controllers/request_passwords_controller.rb
+++ app/controllers/request_passwords_controller.rb
@@ -5,7 +5,7 @@ class RequestPasswordsController < ApplicationController
 # sends reset link
   def create
     user = User.find_by!(email: params[:email])
-    UserMailer.reset_password(user).deliver_later
+    User.send_reset_password_mail(user)
     redirect_to new_session_url, notice: 'reset password link is sent to your email'
 
   rescue ActiveRecord::RecordNotFound
