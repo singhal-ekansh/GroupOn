d5685a53c208c881aeab469b1bf8eee45930a347
Ekansh Singhal <ekansin05@gmail.com>
2023-05-16 14:09:29 +0530

    deals validations

--- db/migrate/20230516073102_create_deals.rb
+++ db/migrate/20230516073102_create_deals.rb
@@ -4,12 +4,12 @@ class CreateDeals < ActiveRecord::Migration[7.0]
       t.string :title, null: false
       t.text :description, null: false
       t.integer :price, null: false
-      t.datetime :start_at
-      t.datetime :expire_at
+      t.datetime :start_at, null: false
+      t.datetime :expire_at, null: false
       t.integer :threshold_value, null: false
       t.integer :total_availaible, null: false
       t.integer :max_per_user
-      t.boolean :published, null:false, default: false
+      t.boolean :published, default: false
       t.integer :count_left
       t.references :user, foreign_key: true
       t.timestamps
