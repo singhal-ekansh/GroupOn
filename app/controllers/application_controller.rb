f0a419a24803d71bf65664907f535b13e9fa546b
Ekansh Singhal <ekansin05@gmail.com>
2023-05-16 12:14:48 +0530

    show logout button or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,18 @@ class ApplicationController < ActionController::Base
 
   before_action :authentication_required
 
+  def current_user
+    @user ||= User.find_by(id: session[:user_id])
+  end
+
   def authentication_required
-    unless User.find_by(id: session[:user_id])
+    unless @user
       redirect_to new_session_url, alert: 'Login to continue'
     end
   end
 
   def already_authenticated
-    if User.find_by(id: session[:user_id])
+    if @user
       # redirect_to , alert: 'Already logged in'
     end
   end
