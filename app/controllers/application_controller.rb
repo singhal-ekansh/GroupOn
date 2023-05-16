3ed548183a68f46146ca7dedf0356a837594fe11
Ekansh Singhal <ekansin05@gmail.com>
2023-05-16 12:59:32 +0530

    error fixed for logout show/hide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,17 @@ class ApplicationController < ActionController::Base
   before_action :authentication_required
 
   def current_user
-    @user ||= User.find_by(id: session[:user_id])
+    User.find_by(id: session[:user_id])
   end
 
   def authentication_required
-    unless @user
+    unless current_user
       redirect_to new_session_url, alert: 'Login to continue'
     end
   end
 
   def already_authenticated
-    if @user
+    if current_user
       # redirect_to , alert: 'Already logged in'
     end
   end
