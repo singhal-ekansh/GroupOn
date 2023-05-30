fb1f9f363ca6169713b871239c41de9a4645dd43
Ekansh Singhal <ekansin05@gmail.com>
2023-05-30 14:01:15 +0530

    token for request mail

--- app/controllers/request_passwords_controller.rb
+++ app/controllers/request_passwords_controller.rb
@@ -1,26 +1,17 @@
 class RequestPasswordsController < ApplicationController
-  before_action :set_user, only: [:create]
-  before_action :generate_token, only: [:create]
+
   def new
   end
 
 # sends reset link
   def create
-    if User.verified.include?(@user)
-      @user.send_reset_password_mail(@token)
+    @user = User.verified.find_by(email: params[:email])
+    if @user
+      @user.create_reset_password_mail
       redirect_to root_url, notice: 'reset password link is sent to your email'
     else
       redirect_to new_request_password_url, alert: 'invalid email address'
     end 
   end 
 
-  private def set_user
-    @user = User.find_by(email: params[:email])  
-  end
-
-  private def generate_token
-    if @user 
-      @token = @user.generate_reset_password_token
-    end
-  end
 end
