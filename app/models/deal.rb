a55d1319b7277e264d75ff705cfbd6add0566d74
Ekansh Singhal <ekansin05@gmail.com>
2023-05-30 11:49:31 +0530

    reviewed changes

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,11 +1,11 @@
 class Deal < ApplicationRecord
   validates :title, presence: true
 
-  with_options if: :published? do |deal|
+  with_options if: -> { published } do |deal|
     deal.validates :description, :price, :start_at, :expire_at, :threshold_value, :total_availaible, presence: true
-    deal.validates :expire_at, comparison: { greater_than: :start_at }, allow_blank: true, if: -> { !!start_at }
+    deal.validates :expire_at, comparison: { greater_than: :start_at }, allow_blank: true, if: -> { start_at }
     deal.validates :price, :threshold_value, :total_availaible, :max_per_user, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
-    deal.validates :threshold_value, comparison: { less_than_or_equal_to: :total_availaible }, allow_blank: true, if: -> { !!total_availaible }
+    deal.validates :threshold_value, comparison: { less_than_or_equal_to: :total_availaible }, allow_blank: true, if: -> { total_availaible }
   end
   
   validate :ensure_published_by_admin
@@ -19,7 +19,4 @@ class Deal < ApplicationRecord
     end
   end
 
-  private def published?
-    !!published
-  end
 end
