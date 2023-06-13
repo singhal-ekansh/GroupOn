e7e4615d5f258260d40f22e54a0e34a225dcbadf
Ekansh Singhal <ekansin05@gmail.com>
2023-06-13 14:57:49 +0530

    verify payment job

--- app/jobs/verify_payment_job.rb
+++ app/jobs/verify_payment_job.rb
@@ -1,12 +1,15 @@
 class VerifyPaymentJob < ApplicationJob
   def perform(checkout_id)
-    checkout_session = Stripe::Checkout::Session.retrieve(checkout_id)
-    order = Order.find_by(id: checkout_session.metadata[:order_id])
-    if order&.pending?
-      deal = order.deal
-      deal.update_columns(qty_sold: deal.qty_sold - order.quantity)
-      order.payment_transactions.create( stripe_id: checkout_session.id, order_id: order.id, status: :failed )
-      order.update_columns(status: :canceled)
+    begin
+      checkout_session = Stripe::Checkout::Session.retrieve(checkout_id)
+      order = Order.find_by(id: checkout_session.metadata[:order_id])
+      if order&.pending?
+        deal = order.deal
+        deal.update_columns(qty_sold: deal.qty_sold - order.quantity)
+        order.payment_transactions.create( stripe_id: checkout_session.id, order_id: order.id, status: :failed )
+        order.update_columns(status: :canceled)
+      end
+    rescue Stripe::InvalidRequestError
     end
   end
 end
