1ea300e36748fcc880507e630c786280a462ce4c
Ekansh Singhal <ekansin05@gmail.com>
2023-06-15 02:10:54 +0530

    order deals validations exracted

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate
   before_action :set_deal, only: [:new, :create]
   before_action :retrieve_checkout_session, only: [:placed, :failed]
+  before_action :set_order, only: [:placed, :failed]
 
   def new
     @order = Order.new
@@ -12,17 +13,12 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(deal_id: params[:deal_id], user_id:  current_user.id, quantity: params[:order][:quantity],amount: params[:order][:quantity].to_i * @deal.price)
+    @order = Order.new(deal_id: params[:deal_id], user_id:  current_user.id, quantity: params[:order][:quantity])
 
     if @order.save
       @deal.update_columns(qty_sold: @deal.qty_sold + @order.quantity)
-
-      @checkout_session = Stripe::Checkout::Session.create(success_url: order_success_url, cancel_url: order_failed_url,
-        line_items: [{ price_data: { currency: 'inr', unit_amount: @deal.price * 100, product_data: {name: @deal.title} } ,
-           quantity: @order.quantity }], mode: 'payment', metadata: { order_id: @order.id })
-       
+      @checkout_session = @order.generate_stripe_session( order_success_url, order_failed_url )
       session[:checkout_session] = @checkout_session.id  
-
       redirect_to @checkout_session.url, allow_other_host: true
     else
       render :new
@@ -30,18 +26,16 @@ class OrdersController < ApplicationController
   end
 
   def placed
-    @order = Order.find_by(id: @checkout_session.metadata[:order_id])
     @order.payment_transactions.create( stripe_id: @checkout_session.payment_intent, status: :paid )
-    @order.update(status: :paid)
+    @order.update_columns(status: :paid)
     redirect_to orders_path, notice: "Order placed"
   end
 
   def failed
-    @order = Order.find_by(id: @checkout_session.metadata[:order_id])
     @order.deal.update_columns(qty_sold: @order.deal.qty_sold - @order.quantity)
     @order.payment_transactions.create( stripe_id: @checkout_session.id, status: :failed )
-    @order.update(status: :canceled)
-    redirect_to deals_path, alert: "payment failed"
+    @order.update_columns(status: :canceled)
+    redirect_to orders_path, alert: "payment failed"
   end
 
   private def set_deal
@@ -49,10 +43,14 @@ class OrdersController < ApplicationController
     redirect_to deals_path, alert: 'invalid deal' if !@deal
   end
 
+  private def set_order
+    @order = Order.find_by(id: @checkout_session.metadata[:order_id])
+  end
+
   private def retrieve_checkout_session
     @checkout_session = Stripe::Checkout::Session.retrieve(session[:checkout_session]) if session[:checkout_session]
-    if !@checkout_session || !@checkout_session.status.eql?('open')
-      redirect_to deals_path, alert: 'session expired'
+    if !@checkout_session || !@checkout_session.status.eql?('complete')
+      redirect_to root_path, alert: 'session expired'
     end
     session[:checkout_session] = nil
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,10 @@
 class Order < ApplicationRecord
   validates :quantity, :amount, :status, presence: true
-  validate :deal_validations
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :check_deal_availaible, if: [:quantity, :deal]
+  validate :check_deal_published, if: :deal
+  validate :check_deal_live, if: :deal
+  validate :check_max_deal_per_user, if: [:quantity, :deal]
 
   belongs_to :user
   belongs_to :deal
@@ -8,11 +12,33 @@ class Order < ApplicationRecord
 
   enum :status, [:pending, :paid, :processed, :canceled]
 
-  def deal_validations
+  before_validation :set_amount, if: :quantity_changed?
+  
+  def generate_stripe_session(success_url, cancel_url)
+    Stripe::Checkout::Session.create(success_url: success_url, cancel_url: cancel_url,
+      line_items: [{ price_data: { currency: 'inr', unit_amount: deal.price * 100, product_data: {name: deal.title} } ,
+         quantity: quantity }], mode: 'payment', metadata: { order_id: id })
+  end
+
+  private def set_amount
+    self.amount = quantity * deal.price
+  end
+
+  private def check_deal_availaible
     errors.add(:quantity, 'exceed more than availaible deals') if quantity > (deal.total_availaible - deal.qty_sold)
-    errors.add(:base, 'order can be placed only for published deals') unless deal.published
-    errors.add(:base, 'order can be placed only for live deals') unless Time.now.between?(deal.start_at, deal.expire_at)
-    quantity_already_purchased = deal.orders.where(user_id: user_id).where(status: :paid).sum(:quantity)
+  end
+
+  private def check_deal_published
+    errors.add(:base, 'order can be placed only for published deals') if !deal.published
+  end
+
+  private def check_deal_live
+    errors.add(:base, 'order can be placed only for live deals') if !Time.now.between?(deal.start_at, deal.expire_at)
+  end
+
+  private def check_max_deal_per_user
+    quantity_already_purchased = deal.orders.where(user_id: user_id).where(status: [:paid, :pending]).sum(:quantity)
     errors.add(:base, 'quantity exceed more than maximum allowed per user') if quantity_already_purchased + quantity > deal.max_per_user
   end
+
 end
