f456fce304cd46c58cc7156643fb27064134ed5d
Ekansh Singhal <ekansin05@gmail.com>
2023-06-26 01:28:24 +0530

    more optimise

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -4,21 +4,21 @@ class DealsController < ApplicationController
   before_action :set_like_dislike_count, only: [:index, :expired_deals, :search]
 
   def index
-    @deals = Deal.includes(:likes, [images: [file_attachment: :blob]], :locations).published.live.paginate(page: params[:page])
+    @deals = Deal.includes([images: [file_attachment: :blob]]).published.live.paginate(page: params[:page])
   end
 
   def show
   end
 
   def expired_deals
-    @deals = Deal.includes(:likes, :images, :locations).published.expired.paginate(page: params[:page])
+    @deals = Deal.includes([images: [file_attachment: :blob]]).published.expired.paginate(page: params[:page])
   end
 
   def search
-    @deals = Deal.includes(:likes, :images, :locations).published.paginate(page: params[:page])
+    @deals = Deal.includes([images: [file_attachment: :blob]]).joins(:locations).published.paginate(page: params[:page]).distinct
     @deals = params[:deal_type] == 'live' ? @deals.live : @deals.expired
 
-    @deals = @deals.search_by_city_and_title("%#{params[:query]}%") if !params[:query].blank?
+    @deals = @deals.search_by_city_and_title("%#{Deal.sanitize_sql_like(params[:query])}%") if !params[:query].blank?
     @deals = @deals.filter_by_category(params[:category]) if !params[:category].blank?
 
     render params[:deal_type] == 'live' ? :index : :expired_deals
@@ -27,6 +27,7 @@ class DealsController < ApplicationController
   private def set_like_dislike_count
     @likes_count = Deal.joins(:likes).published.group(:id).sum('likes.is_liked = true')
     @dislikes_count = Deal.joins(:likes).published.group(:id).sum('likes.is_liked = false')
+    @user_likes_dislikes = current_user.likes.where(likable_type: 'Deal').pluck(:likable_id, :is_liked).to_h
   end
 
   private def set_deal
